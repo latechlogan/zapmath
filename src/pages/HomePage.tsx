673fe84444364535ede785344f99b85fbaabefc5
Logan Baugh <loganbaugh@Logans-MacBook-Pro.local>
2026-06-25 23:03:56 -0500

    feat: collect and pass user input in HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,67 @@
+import { useState } from "react";
+import { type GameConfig } from "../utils/types";
+import { useNavigate } from "react-router";
+
 function HomePage() {
-  return <h1>HomePage works!</h1>;
+  const navigate = useNavigate();
+  const [playerCount, setPlayerCount] = useState<1 | 2>(1);
+  const [target, setTarget] = useState(10);
+
+  const handleStart = (e: React.SubmitEvent) => {
+    e.preventDefault();
+    navigate("/game", {
+      state: {
+        playerCount,
+        target,
+        mode: "firstToX",
+      } satisfies GameConfig,
+    });
+  };
+
+  const handlePlayerCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPlayerCount(Number(e.target.value) as 1 | 2);
+  };
+
+  const handleTarget = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTarget(Number(e.target.value));
+  };
+
+  return (
+    <div>
+      <h1>HomePage works!</h1>
+      <form onSubmit={handleStart}>
+        <input
+          type="radio"
+          name="playerCount"
+          id="onePlayer"
+          value={1}
+          onChange={handlePlayerCount}
+          checked
+        />
+        <label htmlFor="onePlayer">1 Player</label>
+
+        <input
+          type="radio"
+          name="playerCount"
+          id="twoPlayer"
+          value={2}
+          onChange={handlePlayerCount}
+        />
+        <label htmlFor="twoPlayer">2 Player</label>
+
+        <label htmlFor="target">Choose a target score:</label>
+        <input
+          type="text"
+          name="target"
+          id="target"
+          placeholder="10"
+          onChange={handleTarget}
+        />
+
+        <button type="submit">Start New Game</button>
+      </form>
+    </div>
+  );
 }
 
 export default HomePage;
